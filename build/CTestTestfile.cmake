a193975ca29ffea3c6dcc9bb3a856606e6d1be50
ScriptLineStudios <scriptlinestudios@protonmail.com>
2023-04-22 17:07:25 +0200

    rect test

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/scriptline/programming/waphics.c
+# Build directory: /home/scriptline/programming/waphics.c/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ build/tests/CMakeFiles/test_setup.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for test_setup.
+# This may be replaced when dependencies are built.
