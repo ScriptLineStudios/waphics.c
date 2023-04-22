a89e8181e9cee51396f6c9eff0469d28121f2471
ScriptLine <85095943+ScriptLineStudios@users.noreply.github.com>
2023-04-22 17:10:30 +0200

    Delete test.c

--- test.c
+++
@@ -1,9 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "src/include/external/stbi_image.h"
-
-int main() {
-    int width, height, channels;
-    uint32_t *pixels = (uint32_t *)stbi_load("/home/scriptline/programming/C/assets/block1.png" , &width, &height, &channels, 0);
-    printf("%p\n", pixels);
-    return 0;
-}
